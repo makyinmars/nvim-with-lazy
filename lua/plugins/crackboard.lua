e066710371fbcab97201ac23b77dafce165d239e
Franklin <franklinjaradev@gmail.com>
2024-11-29 17:36:41 -0500

    clean up

--- lua/plugins/crackboard.lua
+++ lua/plugins/crackboard.lua
@@ -1,9 +1,10 @@
-return {
-  "boganworld/crackboard.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("crackboard").setup({
-      session_key = os.getenv("CRACKBOARD_SESSION_KEY"),
-    })
-  end,
-}
+-- return {
+--   "boganworld/crackboard.nvim",
+--   dependencies = { "nvim-lua/plenary.nvim" },
+--   config = function()
+--     require("crackboard").setup({
+--       session_key = os.getenv("CRACKBOARD_SESSION_KEY"),
+--     })
+--   end,
+-- }
+return {}
