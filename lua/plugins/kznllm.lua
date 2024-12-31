2d239a29b66ce2d16062481c9d339914f9b7e1d5
Franklin <franklinjaradev@gmail.com>
2024-12-31 00:05:22 -0500

    update

--- lua/plugins/kznllm.lua
+++ lua/plugins/kznllm.lua
@@ -1,19 +1,41 @@
 return {
-  "makyinmars/kznllm.nvim",
+  "chottolabs/kznllm.nvim",
   dependencies = {
     { "j-hui/fidget.nvim" },
   },
   config = function(_)
     local presets = require("kznllm.presets.basic")
 
-    vim.keymap.set("n", "<leader>kdh", function()
-      local buffer_manager = require("kznllm.buffer").buffer_manager
-      buffer_manager:create_sidebar_buffer()
-    end, { desc = "Open LLM History Sidebar" })
-
     -- Helper function to create keymaps with common options
     local function create_keymap(mode, lhs, rhs, desc)
       vim.keymap.set(mode, lhs, rhs, { desc = desc })
+    end
+
+    local function yap_generator()
+      math.randomseed(os.time())
+      local yap_cycle = {
+        "yapped for %ds",
+        "...",
+      }
+
+      local idx = math.random(1, #yap_cycle)
+      return function()
+        idx = idx + 1
+        if idx > #yap_cycle then
+          idx = 1
+        end
+        return yap_cycle[idx]
+      end
+    end
+
+    local yap = yap_generator()
+
+    local function progress_fn(state)
+      local now = os.time()
+      if (now ~= state.last_updated) and ((now - state.start) % 3) == 0 then
+        state.last_updated = now
+        return yap()
+      end
     end
 
     -- Helper function to invoke presets with options
@@ -32,13 +54,13 @@ return {
     create_keymap(
       { "n", "v" },
       "<leader>kdd",
-      invoke_preset_with_options({ debug = true }),
+      invoke_preset_with_options({ debug = true, progress_message_fn = progress_fn }),
       "Send current selection to LLM debug"
     )
     create_keymap(
       { "n", "v" },
       "<leader>kdb",
-      invoke_preset_with_options({ debug = false }),
+      invoke_preset_with_options({ debug = false, progress_message_fn = progress_fn }),
       "Send current selection to LLM llm_fill"
     )
 
