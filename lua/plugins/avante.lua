43eb3b0fa5167f81a5896cc6d9243ee2c6c24080
Franklin <franklinjaradev@gmail.com>
2025-01-09 03:01:48 -0500

    update avante with dual boost

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,6 +1,7 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  lazy = false,
   opts = {
     -- provider = "copilot",
     -- auto_suggestions_provider = "copilot",
@@ -10,6 +11,24 @@ return {
     provider = "claude",
     windows = {
       width = 40,
+    },
+    ---Specify the special dual_boost mode
+    ---1. enabled: Whether to enable dual_boost mode. Default to false.
+    ---2. first_provider: The first provider to generate response. Default to "openai".
+    ---3. second_provider: The second provider to generate response. Default to "claude".
+    ---4. prompt: The prompt to generate response based on the two reference outputs.
+    ---5. timeout: Timeout in milliseconds. Default to 60000.
+    ---How it works:
+    --- When dual_boost is enabled, avante will generate two responses from the first_provider and second_provider respectively.
+    --- Then use the response from the first_provider as provider1_output and the response from the second_provider as provider2_output.
+    --- Finally, avante will generate a response based on the prompt and the two reference outputs, with the default Provider as normal.
+    ---Note: This is an experimental feature and may not work as expected.
+    dual_boost = {
+      enabled = false,
+      first_provider = "cerebras",
+      second_provider = "groq",
+      prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      timeout = 60000, -- Timeout in milliseconds
     },
     vendors = {
       ollama = {
@@ -21,8 +40,10 @@ return {
       deepseek = {
         __inherited_from = "openai",
         api_key_name = "DEEPSEEK_API_KEY",
-        endpoint = "https://api.deepseek.com",
+        endpoint = "https://api.deepseek.com/beta",
         model = "deepseek-chat",
+        temperature = 0,
+        max_tokens = 8192,
       },
       groq = {
         __inherited_from = "openai",
@@ -46,6 +67,7 @@ return {
     --- The below dependencies are optional,
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
