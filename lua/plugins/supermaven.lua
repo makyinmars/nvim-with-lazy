e374ce391597770b4c57f41ff3f79e36b13d0fa6
Franklin <franklinjaradev@gmail.com>
2025-01-11 19:17:39 -0500

    update

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,15 +1,12 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  event = "InsertEnter",
-  cmd = {
-    "SupermavenUseFree",
-    "SupermavenUsePro",
-  },
-  opts = {
-    keymaps = {
-      accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
-    },
-    disable_inline_completion = vim.g.ai_cmp,
-    ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
-  },
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<C-y>",
+      },
+      ignore_filetypes = { env = true },
+      disable_inline_completion = false, -- disables inline completion for use with cmp
+    })
+  end,
 }
