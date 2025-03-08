a7a78c584daa0bbad70f7fd2ffdef7315c964fda
Franklin <franklinjaradev@gmail.com>
2025-03-08 01:06:36 -0500

    update

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,15 +1,12 @@
 return {
   "supermaven-inc/supermaven-nvim",
   config = function()
-    local function enable()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<C-y>",
-        },
-        ignore_filetypes = { env = true },
-        disable_inline_completion = false, -- disables inline completion for use with cmp
-      })
-    end
-    vim.keymap.set("n", "<leader>ct", enable, { desc = "Supermaven Enable" })
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<C-y>",
+      },
+      ignore_filetypes = { env = true },
+      disable_inline_completion = false, -- disables inline completion for use with cmp
+    })
   end,
 }
