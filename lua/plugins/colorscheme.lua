5d7beba8bcac726698cc2205fca0fe4997d83f00
Franklin <franklin-in-mars@pm.me>
2026-02-17 03:26:18 -0500

    update config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,31 +9,45 @@ return {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      terminal_colors = true,
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      invert_intend_guides = false,
-      inverse = true,
-      contrast = "hard",
-      dim_inactive = false,
-      transparent_mode = false,
-    },
+    opts = function()
+      local palette = require("gruvbox").palette
+
+      return {
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        inverse = true,
+        contrast = "hard",
+        dim_inactive = false,
+        transparent_mode = false,
+        overrides = {
+          CursorLine = { bg = palette.dark0_soft },
+          CursorLineNr = { fg = palette.bright_yellow, bold = true },
+          NormalFloat = { bg = palette.dark0_soft },
+          FloatBorder = { fg = palette.dark3, bg = palette.dark0_soft },
+          Pmenu = { bg = palette.dark0_soft },
+          PmenuSel = { fg = palette.dark0, bg = palette.bright_blue, bold = true },
+          Search = { fg = palette.dark0, bg = palette.bright_yellow, bold = true },
+          IncSearch = { fg = palette.dark0, bg = palette.bright_orange, bold = true },
+          Visual = { bg = palette.dark2 },
+        },
+      }
+    end,
     config = function(_, opts)
-      require("gruvbox").setup(opts)
       vim.o.background = "dark"
+      require("gruvbox").setup(opts)
     end,
   },
 }
