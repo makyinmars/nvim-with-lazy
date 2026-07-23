f22b16a15e6cfa507ef3e0949ce3f85ee30829fd
Franklin <franklin-in-mars@pm.me>
2026-07-22 23:55:58 -0400

    Adding Orbs

---
+++ lua/plugins/amp-orbs.lua
@@ -0,0 +1,10 @@
+return {
+  dir = "/Users/franklin/Development/NVIM/amp-orbs.nvim",
+  name = "amp-orbs.nvim",
+  main = "amp_orbs",
+  cmd = "AmpOrbs",
+  keys = {
+    { "<leader>ao", "<cmd>AmpOrbs<cr>", desc = "View Amp Orbs" },
+  },
+  opts = {},
+}
